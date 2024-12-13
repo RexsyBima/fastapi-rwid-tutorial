ca25d2c4df6f1153312ae850e49136119ff31b4e
Rexsxy Bima <Rexsy.Bimq12@gmail.com>
2024-12-13 20:35:26 +0700

    added unfinished auth feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
 from .models import new
 
 data = new()
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 from . import routing  # noqa
--- app/models.py
+++ app/models.py
@@ -45,3 +45,12 @@ def new() -> list[Todo]:
 
 def new_todo(id: int) -> Todo:
     return Todo(id=id, title=f"Todo {id}", is_important=True, date=datetime.now())
+
+
+users: list[User] = [
+    User(username="user1", password="user1", login_at=datetime.now()),
+    User(username="user2", password="user2", login_at=datetime.now()),
+    User(username="user3", password="user3", login_at=datetime.now()),
+    User(username="user4", password="user4", login_at=datetime.now()),
+    User(username="user5", password="user5", login_at=datetime.now()),
+]
